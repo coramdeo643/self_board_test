59ac66f83dcbba417f6e16561cc829999878753b
coramdeo643 <coramdeo643@kakao.com>
2025-10-01 17:15:31 +0900

    fix: core setting fix - Interceptor

--- src/main/java/com/example/demo/__core/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/__core/config/WebMvcConfig.java
@@ -4,15 +4,23 @@ import com.example.demo.__core.intercept.LoginInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 	private final LoginInterceptor loginInterceptor;
 
+	@Override
 	public void addInterceptors(InterceptorRegistry reg) {
 		reg.addInterceptor(loginInterceptor)
-				.addPathPatterns("/board/**", "/user/**", "/reply/**")
-				.excludePathPatterns("/board/{id:\\d+");
+				.addPathPatterns("/**")
+				// API와 update-form만 보호
+				.excludePathPatterns(
+						"/api/users/join",      // 회원가입 API
+						"/api/users/login",     // 로그인 API
+						"/users/join-form",     // 회원가입 페이지
+						"/users/login-form"     // 로그인 페이지
+				);
 	}
 }
